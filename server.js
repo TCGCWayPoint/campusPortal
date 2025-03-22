f4c22908e9194211db0e5eb317070bb8cafdc956
TCGCWayPoint <riodanicaave02@gmail.com>
2025-03-22 15:14:03 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if (!mongoURI) {
     process.exit(1);
 }
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
     .then(() => console.log("✅ MongoDB connected successfully"))
     .catch(err => console.error("❌ MongoDB connection error:", err));
 
